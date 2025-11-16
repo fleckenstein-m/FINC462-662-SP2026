818bc4bbcd4cab4986057e7fc1bda7a512d8d607
Matthias Fleckenstein <magicmaze@t-online.de>
2025-11-16 14:36:15 -0500

    Initial commit

---
+++ docs/searchindex.en.js
@@ -0,0 +1,74 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "FINC462/662 - Fixed Income Securities \u003e Assignments",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Assignment 01",
+    "uri": "/FINC462-662-SP2026/assignments/assignment_01/index.html"
+  },
+  {
+    "breadcrumb": "FINC462/662 - Fixed Income Securities \u003e Lectures",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Lecture 01",
+    "uri": "/FINC462-662-SP2026/lectures/lecture_01/index.html"
+  },
+  {
+    "breadcrumb": "FINC462/662 - Fixed Income Securities",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Lectures",
+    "uri": "/FINC462-662-SP2026/lectures/index.html"
+  },
+  {
+    "breadcrumb": "FINC462/662 - Fixed Income Securities \u003e Assignments",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Assignment 02",
+    "uri": "/FINC462-662-SP2026/assignments/assignment_02/index.html"
+  },
+  {
+    "breadcrumb": "FINC462/662 - Fixed Income Securities",
+    "content": "Assignment 01 Assignment 02",
+    "description": "Assignment 01 Assignment 02",
+    "tags": [],
+    "title": "Assignments",
+    "uri": "/FINC462-662-SP2026/assignments/index.html"
+  },
+  {
+    "breadcrumb": "FINC462/662 - Fixed Income Securities \u003e Lectures",
+    "content": "Lectures\rLecture 1 Slides",
+    "description": "Lectures\rLecture 1 Slides",
+    "tags": [],
+    "title": "Lecture 02",
+    "uri": "/FINC462-662-SP2026/lectures/lecture_02/index.html"
+  },
+  {
+    "breadcrumb": "FINC462/662 - Fixed Income Securities",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/FINC462-662-SP2026/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This is your new home page.\n📄 Show Syllabus",
+    "description": "This is your new home page.\n📄 Show Syllabus",
+    "tags": [],
+    "title": "FINC462/662 - Fixed Income Securities",
+    "uri": "/FINC462-662-SP2026/index.html"
+  },
+  {
+    "breadcrumb": "FINC462/662 - Fixed Income Securities",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/FINC462-662-SP2026/tags/index.html"
+  }
+]
